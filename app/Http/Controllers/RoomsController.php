5ac40f575d590dfc0e0ec3e81e904256338a06fe
Nootan Ghimire <nootan.ghimire@gmail.com>
2017-01-15 02:13:51 +0545

    Protect RoomsController with Auth Middleware

--- app/Http/Controllers/RoomsController.php
+++ app/Http/Controllers/RoomsController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 
 class RoomsController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     //GET Requests 
     public function get_details($date){
 
