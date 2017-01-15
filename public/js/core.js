f367071abaa35ff19354b18a8b0db667b012adc5
Nootan Ghimire <nootan.ghimire@gmail.com>
2017-01-15 14:12:58 +0545

    Add frontend UI code (mock)

---
+++ public/js/core.js
@@ -0,0 +1,144 @@
+var App = angular.module("ZenRoomsApp", []);
+
+App.filter('range', function() {
+  return function(input, total) {
+    var date = new Date();
+    var total = parseInt(total);
+
+    for (var i=0; i<total; i++) {
+      input.push(i + (date.getFullYear() - total + 1));
+    }
+
+    return input;
+  };
+});
+
+App.controller("BulkOperationsController", function($scope, $http) {
+
+  //Room Size
+  $scope.roomSizeList = ["Single Room", "Double Rooms"];
+
+  //Days
+  $scope.days = ["Mondays", "Tuesdays", "Wednesdays", "Thursdays", "Fridays", "Saturdays", "Sundays"];
+
+  //$http GET Request
+  //$http.get("URL").then(function(response)){
+  //  response.data
+  //}
+
+  // Set this from backend
+  $scope.bulkOperations = {
+    roomSizeSelected : "Single Room",
+    dateFrom : new Date(),
+    dateTo : new Date(),
+    allDays : false,
+    allWeekdays : false,
+    allWeekends : false,
+    selectedDays : {Mondays:false, Tuesdays:false, Wednesdays: false, Thursdays: false, Fridays: false, Saturdays: false, Sundays: false},
+    changePriceTo : 0,
+    changeAvailabilityTo : 0
+  }
+
+  // Toggle day checkboxes
+  $scope.toggleDaySelection = function(day) {
+    $scope.bulkOperations.selectedDays[day] = !$scope.bulkOperations.selectedDays[day];
+  }
+
+  // Reset Form
+  $scope.cancelEventHandler = function() {
+    $scope.bulkOperations = {
+      roomSizeSelected : "Single Room",
+      dateFrom : new Date(),
+      dateTo : new Date(),
+      allDays : false,
+      allWeekdays : false,
+      allWeekends : false,
+      selectedDays : {Mondays:false, Tuesdays:false, Wednesdays: false, Thursdays: false, Fridays: false, Saturdays: false, Sundays: false},
+      changePriceTo : 0,
+      changeAvailabilityTo : 0
+    }
+  }
+
+  // Save form
+  $scope.submitEventHandler = function() {
+    console.log($scope.bulkOperations);
+  }
+});
+
+App.controller("CalendarViewController", function($scope) {
+  $scope.days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  $scope.months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  
+  // Set this from backend
+  $scope.roomSizeList = ["Single Room", "Double Room", "Triple Room"];
+  
+  $scope.date = new Date();
+  $scope.currentMonth = $scope.months[$scope.date.getMonth()];
+  $scope.currentMonthNum = $scope.months.indexOf($scope.currentMonth);
+  $scope.currentYear = $scope.date.getFullYear().toString();
+
+
+  $scope.monthStart = new Date(parseInt($scope.currentYear), $scope.currentMonthNum, 1);
+  $scope.monthEnd = new Date(parseInt($scope.currentYear), $scope.currentMonthNum + 1, 1);
+  $scope.monthLength = ($scope.monthEnd - $scope.monthStart) / (1000 * 60 * 60 * 24);
+
+  $scope.dayNames = [];
+
+  $scope.dayStartIndex = $scope.monthStart.getDay();
+
+  /*
+  $scope.roomAndPrice = {
+    //Single Room
+    "Single Room" : [{room : 1, price: 1000}, {},..],
+    //Double Room
+    "Double Room" : []
+    //More Arrays For More Rooms
+  }
+  */
+
+  $scope.roomAndPrice = {};
+
+  //Change function name to something like getRoomAndPrice();
+  $scope.getRandomRoomAndPrice = function(year, month){
+    //Get values from 'year' and 'month' from db
+    
+    for(var i = 0; i < $scope.roomSizeList.length; i++) {
+      $scope.roomAndPrice[$scope.roomSizeList[i]] = [];
+      for(var j = 0; j <$scope.monthLength; j++) {
+        $scope.roomAndPrice[$scope.roomSizeList[i]].push({
+
+          //Set Id, room available, and price from backend
+          id:"id-"+i+"-"+j,
+          editRoom: false, // Necessary
+          editPrice: false, // Necessary
+          room: parseInt(Math.random() * 5),
+          price: parseInt(Math.random() * 10000)
+        })
+      }
+    }
+  }
+
+  $scope.updateCalendar = function(){
+    $scope.currentMonthNum = $scope.months.indexOf($scope.currentMonth);
+    $scope.monthStart = new Date(parseInt($scope.currentYear), $scope.currentMonthNum, 0);
+    $scope.monthEnd = new Date(parseInt($scope.currentYear), $scope.currentMonthNum + 1, 0);
+    $scope.monthLength = ($scope.monthEnd - $scope.monthStart) / (1000 * 60 * 60 * 24);
+    $scope.dayStartIndex = $scope.monthStart.getDay();
+
+    $scope.dayNames = [];
+    for(var i = 0; i< $scope.monthLength; i++){
+      if($scope.dayStartIndex > 6) $scope.dayStartIndex = 0;
+      $scope.dayNames.push($scope.days[$scope.dayStartIndex]);
+      $scope.dayStartIndex++;
+    }
+
+    $scope.getRandomRoomAndPrice($scope.currentYear, $scope.currentMonthNum);
+  }
+
+  $scope.updateCalendar();
+
+  $scope.updateCell = function(id, room, price){
+    //Get updated room and price of id. Send to backend
+    console.log(id, room, price);
+  }
+})
---
+++ resources/views/angular-front.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en" ng-app="ZenRoomsApp">
+<head>
+  <meta charset="UTF-8">
+  <title>ZenRooms</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/layout.css">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.1/angular.min.js"></script>
+  <script src="js/core.js"></script>
+</head>
+<body>
+  <div id="main">
+    <div id="bulkOperations" ng-controller="BulkOperationsController">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">Bulk Operations</div>
+        </div>
+        <div class="row">
+          <form action="" class="form-horizontal form-inline">
+            <div class="col-md-1"><strong>Select Room:</strong></div>
+            <div class="col-md-2">
+              <select class = "form-control" name="" id="" ng-model="bulkOperations.roomSizeSelected" >
+                <option ng-repeat="roomSize in roomSizeList" value="{{roomSize}}">{{roomSize}}</option>
+              </select>
+            </div>
+          </form>
+        </div>
+        <div id="daysSection" class="row">
+          <form action="" class="form-horizontal form-inline">
+            <div class="col-md-1"><strong>Select Days:</strong></div>
+            <div class="col-md-3">
+              <div class="row">
+                <div class="col-md-2">From:</div> <input type="date" class="form-control" ng-model="bulkOperations.dateFrom"/>
+              </div>
+              <div class="row">
+                <div class="col-md-2">To:</div> <input type="date" class="form-control" ng-model="bulkOperations.dateTo"/>
+              </div>
+            </div>
+            <div class="col-md-1">Refine Days:</div>
+            <div class="col-md-2">
+              <div class="checkbox">
+                <label><input type="checkbox" ng-model="bulkOperations.allDays"> All Days</label>
+              </div>
+              <div class="checkbox">
+                <label><input type="checkbox" ng-model="bulkOperations.allWeekdays"> All Weekdays</label>
+              </div>
+              <div class="checkbox">
+                <label><input type="checkbox" ng-model="bulkOperations.allWeekends"> All Weekends</label>
+              </div>
+            </div>
+            <div class="col-md-2">
+              <div class="checkbox" ng-repeat="day in days | limitTo:3">
+                <label><input type="checkbox" ng-checked="bulkOperations.selectedDays[day]" ng-click="toggleDaySelection(day)"> {{day}}</label>
+              </div>
+            </div>
+            <div class="col-md-2">
+              <div class="checkbox" ng-repeat="day in days | limitTo:3:3">
+                <label><input type="checkbox" ng-checked="bulkOperations.selectedDays[day]" ng-click="toggleDaySelection(day)"> {{day}}</label>
+              </div>
+            </div>
+            <div class="col-md-1">
+              <div class="checkbox" ng-repeat="day in days | limitTo:1:6">
+                <label><input type="checkbox" ng-checked="bulkOperations.selectedDays[day]" ng-click="toggleDaySelection(day)"> {{day}}</label>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div class="row">
+          <div class="col-md-2"><strong>Change Price To: </strong></div>
+          <div class="col-md-10 form-inline"><input type="text" class="form-control" ng-model="bulkOperations.changePriceTo"></div><br><br>
+          <div class="col-md-2"><strong>Change Availability To: </strong></div>
+          <div class="col-md-10 form-inline"><input type="text" class="form-control" ng-model="bulkOperations.changeAvailabilityTo"></div>
+        </div>
+        <div class="row">
+          <button type="button" class="btn btn-default" ng-click="cancelEventHandler()">Cancel</button>
+          <button type="button" class="btn btn-success" ng-click="submitEventHandler()">Update</button>
+        </div>
+      </div>
+    </div>
+    <div id="calendarView" ng-controller="CalendarViewController">
+      <div class="container-fluid">
+        <div class="room-headings">
+          <table class="">
+            <tr style="height: 44px"><td>&nbsp;</td></tr>
+            <tr><td>Price and Availability</td></tr>
+            <tr><td>&nbsp;</td></tr>
+            <tbody ng-repeat="roomSizeName in roomSizeList">
+              <tr class="roomSizeName"><td>{{roomSizeName}}</td></tr>
+              <tr class="roomAvailable"><td>Rooms Available</td></tr>
+              <tr class="roomPrice"><td>Price</td></tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="year-selector">
+          <select name="" id="" ng-model="currentMonth" ng-change="updateCalendar()" class="form-control">
+            <option  ng-repeat="month in months">{{month}}</option>
+          </select>
+          <select name="" id="" ng-model="currentYear" ng-change="updateCalendar()" class="form-control">
+            <option ng-repeat="year in [] | range:25">{{year}}</option>
+          </select>
+        </div>
+        <div class="room-content">
+          <table class="">
+            <tr><td ng-repeat="day in dayNames track by $index">{{day}}</td></tr>
+            <tr><td ng-repeat="day in dayNames track by $index">{{$index+1}}</td></tr>
+            <tbody ng-repeat="roomSizeName in roomSizeList">
+              <tr class="roomSizeName"><td colspan="{{monthLength}}">&nbsp;</td></tr>
+              <tr class="roomAvailable">
+                <td ng-repeat="roomValues in roomAndPrice[roomSizeName] track by $index">
+                  <div  ng-click="roomValues.editRoom=!roomValues.editRoom">{{roomValues.room}}</div> 
+                  <div class="edit" ng-show="roomValues.editRoom">
+                    <form name="editRoomForm">
+                      <input type="text" name="room" class="form-control form-inline" ng-model="roomAndPrice[roomSizeName][$index].room" ng-model-options="{ updateOn: 'submit' }" ng-change="updateCell(roomValues.id, roomAndPrice[roomSizeName][$index].room, roomAndPrice[roomSizeName][$index].price)"> 
+                      <button class="btn btn-primary" type="submit" ng-click="roomValues.editRoom=!roomValues.editRoom"><i class="fa fa-check" aria-hidden="true"></i></button>
+                      <button class="btn btn-default" ng-click="editRoomForm.room.$rollbackViewValue();roomValues.editRoom=!roomValues.editRoom"><i class="fa fa-times" aria-hidden="true"></i></button>
+                    </form>
+                  </div>
+                </td>
+              </tr>
+              <tr class="roomPrice">
+                <td ng-repeat="roomValues in roomAndPrice[roomSizeName] track by $index">
+                  <div ng-click="roomValues.editPrice=!roomValues.editPrice">{{roomValues.price}} IDR</div> 
+                  <div class="edit" ng-show="roomValues.editPrice">
+                    <form name="editPriceForm">
+                      <input type="text" name="price" class="form-control form-inline" ng-model="roomAndPrice[roomSizeName][$index].price" ng-model-options="{ updateOn: 'submit' }" ng-change="updateCell(roomValues.id, roomAndPrice[roomSizeName][$index].room, roomAndPrice[roomSizeName][$index].price)"> 
+                      <button class="btn btn-primary" ng-click="roomValues.editPrice=!roomValues.editPrice;" type="submit"><i class="fa fa-check" aria-hidden="true"></i>
+                      </button>
+                      <button class="btn btn-default" ng-click="editPriceForm.price.$rollbackViewValue();roomValues.editPrice=!roomValues.editPrice"><i class="fa fa-times" aria-hidden="true"></i></button>
+                    </form>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
